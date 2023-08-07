843500cf037c2e8514905d8c57bc23548ae4eaf7
devendra-alt <devendramulewa171@gmail.com>
2023-08-07 19:34:29 +0530

    Added empty nav

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Header() {
+  return <nav className="main-header-nav"></nav>;
+}
+
+export default Header;
---
+++ src/components/Rockets.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+
+function Rockets() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default Rockets;
