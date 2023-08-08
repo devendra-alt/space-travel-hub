5dd22f8cc40950b50e3f6a1fb0cc4fc42eaa10fa
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-08-08 16:25:52 +0300

    Setup navigation and routes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Rockets from './components/Rockets';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import './styling/App.css';
+import Navbar from './components/Navbar';
+import Rockets from './components/rockets';
 import Missions from './components/Missions';
 import Profile from './components/Profile';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Rockets />,
-  },
-  {
-    path: '/missions',
-    element: <Missions />,
-  },
-  {
-    path: 'my-profile',
-    element: <Profile />,
-  },
-]);
-
-const App = () => <RouterProvider router={router} />;
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <hr />
+      <div className="App-content">
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
 
 export default App;
