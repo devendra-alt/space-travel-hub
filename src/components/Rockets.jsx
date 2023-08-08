051dbde9a99df08f9f173b73c490cdf3726880bb
devendra-alt <devendramulewa171@gmail.com>
2023-08-09 00:33:59 +0530

    Added dispath action on Rocket.jsx components (called once when component being mounted)

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './Header';
+import { useDispatch } from 'react-redux';
+import { fetchRocketData } from '../redux/rockets/features/rocketsSlice';
 
 function Rockets() {
+  const dispath = useDispatch();
+
+  useEffect(() => {
+    dispath(fetchRocketData());
+  }, []);
+
   return (
     <>
       <Header />
