23c0040450845e60f440a0d9b77d05bf24c83a18
devendra-alt <devendramulewa171@gmail.com>
2023-08-09 11:29:28 +0530

    Add MyRocket reserved lists to profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,6 +5,21 @@ function Profile() {
   return (
     <>
       <Header />
+      <Container fluid className="mx-auto col-10">
+        <Row>
+          <Col></Col>
+          <Col>
+            <p className="h2">My Rockets</p>
+            <ListGroup>
+              {reservedRockets.map((rocket) => (
+                <ListGroup.Item>
+                  <p style={{ fontSize: '1.4rem' }}>{rocket.name}</p>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
