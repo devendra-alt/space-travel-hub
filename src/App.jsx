025af78283e510ae85fde5c09ec690ceb3396fae
devendra-alt <devendramulewa171@gmail.com>
2023-08-09 11:27:44 +0530

    Changed router config from App.jsx function to file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,25 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 import Profile from './components/Profile';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchRocketData } from './redux/rockets/features/rocketsSlice';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Rockets />,
+  },
+  {
+    path: '/missions',
+    element: <Missions />,
+  },
+  {
+    path: '/my-profile',
+    element: <Profile />,
+  },
+]);
+function App() {
   const dispath = useDispatch();
   useEffect(() => {
     dispath(fetchRocketData());
