19e5d49e217edee8183031a5724e1794f9935255
devendra-alt <devendramulewa171@gmail.com>
2023-08-09 11:26:50 +0530

    Added fetchRocketData dispath to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,10 @@ import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 import Profile from './components/Profile';
 
-function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Rockets />,
-    },
-    {
-      path: '/missions',
-      element: <Missions />,
-    },
-    {
-      path: '/my-profile',
-      element: <Profile />,
-    },
-  ]);
+  const dispath = useDispatch();
+  useEffect(() => {
+    dispath(fetchRocketData());
+  }, [dispath]);
   return <RouterProvider router={router} />;
 }
 
