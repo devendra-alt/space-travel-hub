8f43ee61afd8471387d61cbcf91fee68df2f623a
devendra-alt <devendramulewa171@gmail.com>
2023-08-09 11:30:45 +0530

    Add Rocket.jsx for single rocket item

---
+++ src/components/Rocket.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Badge, Card, Stack } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import {
+  rocketBooking,
+  rocketBookingCancel,
+} from '../redux/rockets/features/rocketsSlice';
+
+function Rocket({ id, name, images, description, reserved }) {
+  const dispath = useDispatch();
+  return (
+    <Card>
+      <Stack direction="horizontal" gap={2}>
+        <Card.Img src={images[0]} style={{ width: '20rem' }} />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text style={{ fontSize: '1.4rem' }}>
+            {reserved && <Badge bg="success">Reserved</Badge>} {description}
+          </Card.Text>
+          {reserved ? (
+            <Button
+              variant="outline-secondary"
+              onClick={() => dispath(rocketBookingCancel(id))}
+            >
+              Cancel Reservation
+            </Button>
+          ) : (
+            <Button onClick={() => dispath(rocketBooking(id))}>
+              Reserve Rocket
+            </Button>
+          )}
+        </Card.Body>
+      </Stack>
+    </Card>
+  );
+}
+
+export default Rocket;
