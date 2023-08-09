29819016ef8d1328f7f2dd0b07a8aacf5184cf64
devendra-alt <devendramulewa171@gmail.com>
2023-08-09 14:14:37 +0530

    Added empty reservation message on myProfile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,28 +1,49 @@
 import React from 'react';
-import {
-  Col, Container, ListGroup, Row,
-} from 'react-bootstrap';
-import { useSelector } from 'react-redux';
+import { Alert, Button, Col, Container, ListGroup, Row } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import Header from './Header';
+import { rocketBookingCancel } from '../redux/rockets/features/rocketsSlice';
 
 function Profile() {
+  const dispath = useDispatch();
   const { rockets } = useSelector((state) => state.rockets);
   const reservedRockets = rockets.filter((rocket) => rocket.reserved === true);
   return (
     <>
       <Header />
       <Container fluid className="mx-auto col-10">
-        <Row>
+        <Row className="flex-column flex-lg-row flex-xl-row">
           <Col />
           <Col>
-            <p className="h2">My Rockets</p>
-            <ListGroup>
-              {reservedRockets.map((rocket) => (
-                <ListGroup.Item key={rocket.id}>
-                  <p style={{ fontSize: '1.4rem' }}>{rocket.name}</p>
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
+            {!reservedRockets.length && (
+              <Alert variant="info">My Reserved Rockets is empty</Alert>
+            )}
+            {!!reservedRockets.length && (
+              <ListGroup>
+                <p className="h2">My Rockets</p>
+                {reservedRockets.map((rocket) => (
+                  <ListGroup.Item
+                    key={rocket.id}
+                    className="d-flex flex-sm-column flex-md-row flex-lg-row justify-content-between align-items-center"
+                  >
+                    <div>
+                      <p style={{ fontSize: '1.4rem' }}>{rocket.name}</p>
+                      <Button variant="outline-info">
+                        <a href={rocket.wikipedia} target="_blank">
+                          Read more
+                        </a>
+                      </Button>
+                    </div>
+                    <Button
+                      variant="outline-danger"
+                      onClick={() => dispath(rocketBookingCancel(rocket.id))}
+                    >
+                      Cancel Reservation
+                    </Button>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            )}
           </Col>
         </Row>
       </Container>
--- src/components/Rocket.jsx
+++ src/components/Rocket.jsx
@@ -1,51 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Badge, Card, Stack, Button,
-} from 'react-bootstrap';
+import { Badge, Card, Stack, Button } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import {
   rocketBooking,
   rocketBookingCancel,
 } from '../redux/rockets/features/rocketsSlice';
 
-function Rocket({
-  id, name, images, description, reserved,
-}) {
+function Rocket({ id, name, images, description, reserved }) {
   const dispath = useDispatch();
   return (
-    <Card>
-      <Stack direction="horizontal" gap={2}>
-        <Card.Img src={images[0]} style={{ width: '20rem' }} />
-        <Card.Body>
-          <Card.Title>{name}</Card.Title>
-          <Card.Text style={{ fontSize: '1.4rem' }}>
-            {reserved && <Badge bg="success">Reserved</Badge>}
-            {' '}
-            {description}
-          </Card.Text>
-          {reserved ? (
-            <Button
-              variant="outline-secondary"
-              onClick={() => dispath(rocketBookingCancel(id))}
-            >
-              Cancel Reservation
-            </Button>
-          ) : (
-            <Button onClick={() => dispath(rocketBooking(id))}>
-              Reserve Rocket
-            </Button>
-          )}
-        </Card.Body>
-      </Stack>
+    <Card className="d-flex flex-sm-column flex-md-row flex-lg-row">
+      <Card.Img src={images[0]} style={{ width: '20rem' }} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text style={{ fontSize: '1.4rem' }}>
+          {reserved && <Badge bg="success">Reserved</Badge>} {description}
+        </Card.Text>
+        {reserved ? (
+          <Button
+            variant="outline-secondary"
+            onClick={() => dispath(rocketBookingCancel(id))}
+          >
+            Cancel Reservation
+          </Button>
+        ) : (
+          <Button onClick={() => dispath(rocketBooking(id))}>
+            Reserve Rocket
+          </Button>
+        )}
+      </Card.Body>
     </Card>
   );
 }
 
 Rocket.propTypes = {
-  id: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
-  images: PropTypes.shape.isRequired,
+  images: PropTypes.array.isRequired,
   description: PropTypes.string.isRequired,
   reserved: PropTypes.bool,
 };
