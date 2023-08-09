0b2ab7213eea7cb9a8453fbcca7d40a1e335d7b8
devendra-alt <devendramulewa171@gmail.com>
2023-08-09 11:36:49 +0530

    Added error or loading components on diffrent redux states

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,18 +1,23 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Header from './Header';
-import { fetchRocketData } from '../redux/rockets/features/rocketsSlice';
-
+import Rocket from './Rocket';
+import { Alert, ListGroup, Spinner } from 'react-bootstrap';
 function Rockets() {
-  const dispath = useDispatch();
-
-  useEffect(() => {
-    dispath(fetchRocketData());
-  }, [dispath]);
-
+  const { rockets, loading, error } = useSelector((state) => state.rockets);
   return (
     <>
       <Header />
+      <section className="rockets-container">
+        {loading === 'pending' && (
+          <Spinner
+            animation="border"
+            role="status"
+            style={{ width: '5rem', height: '5rem' }}
+            variant="primary"
+          ></Spinner>
+        )}
+        {error && <Alert variant="danger">{error}</Alert>}
         <ListGroup>
           {rockets.map((rocket) => (
             <ListGroup.Item key={rocket.id}>
